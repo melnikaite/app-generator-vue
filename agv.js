61d7b2e8f83165876e969bcde5c30685674c0ee4
Petr Reshetin <preshetin@gmail.com>
2018-04-22 12:06:56 +0300

    Multiple files generation start

--- agv.js
+++ agv.js
@@ -4,18 +4,36 @@ var argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 
 // node agv.js --model Book --fields '[{"name":"string"},{"location":"bytes32"}]'
-var createFromTemplate = require('./createFromTemplate.js');
+var generatorContract = require('./generatorContract.js');
 
 const targetEntityName = argv.model;
 const targetEntityFields = argv.fields;
 
-let fileInitialString = fs.readFileSync('contracts/TemplateContract.sol.tmpl', 'utf8');
+const generators = [
+  {
+    name: 'contract',
+    templatePath: 'contracts/TemplateContract.sol.tmpl',
+    resultFilePath: 'contracts/' + targetEntityName + 'Contract.sol'
+  },
+  {
+    name: 'itemJs',
+    templatePath: 'src/js/item.js.tmpl',
+    resultFilePath: 'src/js/' + targetEntityName.toLowerCase() + '.js'
+  }
+];
 
-let resultString = createFromTemplate.createContract(fileInitialString, targetEntityName, targetEntityFields);
-
-const createdContractFilePath = 'contracts/' + targetEntityName + 'Contract.sol';
-
-fs.appendFileSync(createdContractFilePath, resultString);
-// console.log(resultString);
-
-console.log("Generated contract: " + createdContractFilePath);
+generators.forEach(generator => {
+  switch (generator.name) {
+    case 'contract':
+      let fileInitialString = fs.readFileSync(generator.templatePath, 'utf8');
+      let resultString = generatorContract.createContract(fileInitialString, targetEntityName, targetEntityFields);
+      fs.appendFileSync(generator.resultFilePath, resultString);
+      console.log("✅ Generated contract: " + generator.resultFilePath);
+      break;
+    case 'itemJs':
+      console.log('🙏 Help wanted! ' + generator.resultFilePath + ' needs to be generated... ');
+      break;
+    default:
+      console.log('NOT FOUND!');
+  }
+});
