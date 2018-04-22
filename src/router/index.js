31c4e3930de326a4aafb138c78cb602b3a442a5f
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2018-04-22 14:36:23 +0300

    remove hardcoded

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Item from '@/components/Item'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/items',
-      name: 'items',
-      component: Item
-    }
+
   ]
 })
